ce5891fcf746eb40801faac593790c87b69e0cdd
meganzz <zengmegan@berkeley.edu>
2019-10-13 14:17:49 -0700

    :art: got rid of artifacts

--- hardware/controller/controller.ino
+++ hardware/controller/controller.ino
@@ -6,12 +6,43 @@
 #define LANDING 4
 #define FINISHED 5
 
-int flightStatus = PRELAUNCH;
+int currentStatus = PRELAUNCH;
 double flightTime = 0;
 char leftBound;
-bracketEnded = true;
 
-int pollStatus(currentStatus); //reads the next two serial inputs and returns if valid format
+int pollStatus() {
+  int flightStatus = currentStatus;
+  char statusChar = Serial.read();
+  char rightBound = Serial.read();
+  if (rightBound == ',') {
+    if (statusChar == 'A') {
+      flightStatus = LIFTOFF;
+    }
+    else if (statusChar == 'D') {
+      flightStatus = MGRAV_BEGIN;
+    }
+    else if (statusChar == 'F') {
+      flightStatus = MGRAV_END;
+    }
+    else if (statusChar == 'H') {
+      flightStatus = LANDING;
+    }
+    else if (statusChar == 'J') {
+      flightStatus = FINISHED;
+    }
+  }
+  return flightStatus;
+}
+
+double experimentTime() {
+  char mark = Serial.read();
+  String time = "";
+  while (mark != ',') {
+    time += mark;
+    mark = Serial.read();
+  }
+  return time.toDouble();
+}
 
 void setup() {
   Serial.begin(115200);     // opens serial port, sets baudrate to 115200 bps
@@ -19,75 +50,16 @@ void setup() {
 }
 
 void loop() {
-<<<<<<< HEAD
   while (Serial.available()) {
     if (Serial.available() > 0) {
-      currentChar = Serial.read();
+      char currentChar = Serial.read();
       if (currentChar == '[') {
-        bracketEnded = false;
-        flightStatus = pollStatus(flightStatus);
-      } else if (currentChar == ']') {
-        bracketEnded = true;
-=======
-  while (Serial.available()){
-    if (Serial.available() > 0){
-      leftBound = Serial.read();
-      if (leftBound == '['){
-        char status = Serial.read();
-        char rightBound = Serial.read();
-        if (rightBound == ','){
-          if (status == 'A'){
-            flightStatus = LIFTOFF;
-          }
-          else if (status == 'D'){
-            flightStatus = MGRAV_BEGIN;
-          }
-          else if (status == 'F'){
-            flightStatus = MGRAV_END;
-          }
-          else if (status == 'H'){
-            flightStatus = LANDING;
-          }
-          else if (status == 'J'){
-            flightStatus = FINISHED;
-          }
-          char mark = Serial.read();
-          String time = "";
-          while (mark != ',') {
-            time += mark;
-            mark = Serial.read();
-          }
-          flightTime = time.toDouble();
-        }
->>>>>>> edff02d93aaa2de5c5394668bf8bf85b61a32dfe
+        currentStatus = pollStatus();
+        flightTime = experimentTime();
       }
     }
   }
 }
 
-<<<<<<< HEAD
-int pollStatus(currentStatus) {
-  char statusChar = Serial.read();
-  char rightBound = Serial.read();
-  if (rightBound == ',') {
-    if (statusChar == 'A') {
-      currentStatus = LIFTOFF;
-    }
-    else if (statusChar == 'D') {
-      currentStatus = MGRAV_BEGIN;
-    }
-    else if (statusChar == 'F') {
-      currentStatus = MGRAV_END;
-    }
-    else if (statusChar == 'H') {
-      currentStatus = LANDING;
-    }
-    else if (statusChar == 'J') {
-      currentStatus = FINISHED;
-    }
-  }
-  return currentStatus;
-=======
 
->>>>>>> edff02d93aaa2de5c5394668bf8bf85b61a32dfe
-}
+
