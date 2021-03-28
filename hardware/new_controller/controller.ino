059447fcbe4f2f9c9f81a936963e5fed3eaf065a
jason bustani <jason.bustani@berkeley.edu>
2021-03-28 14:15:19 -0700

    finished everything except the motors, going to start testing

--- hardware/new_controller/controller.ino
+++ hardware/new_controller/controller.ino
@@ -1,28 +1,23 @@
-#define FIVE_VOLTS 972.0
-#define HEATER A5;
-#define TEMP_SENSOR_1 A0;
 #include <SPI.h>
 #include <SD.h>
+#include "definitions.h"
 
-/**
- * Data structure that describes the states of the machine.
- * PREFLIGHT: subroutines include temp. check and data writes to SD
- * MICROG: Run the experiment, while also maintaing temperature and data writes to SD
- * POSTFLIGHT: TBD
-*/
+
 typedef enum {
     PREFLIGHT,
     MICROG,
     POSTFLIGHT
 } STATES;
-/**
- * Variable that will hold the 200 byte packets
- */
+
+
 char current_packet[200];
-/**
- * temp_reg takes in no arguments and determines if
- * the temperature needs to be raised.
- */
+
+File log_file;
+/**Motor pins */
+const int STEP; //step input
+const int DIR; //direction input
+const int nSLEEP; //sleep mode input
+
 bool temp_reg() {
     // Should change to <= because of schematic
     bool state_ok = (analogRead(TEMP_SENSOR_1) >= FIVE_VOLTS / 2.0);
@@ -31,22 +26,53 @@ bool temp_reg() {
     else 
     digitalWrite(HEATER, LOW);
 }
-/** read_temp takes a pin and returns the translation from voltage 
- * to Farhenheit degrees
- */
+
+
 float read_temp() {
   return (1.0 / (1.0/298.15 + 1.0/3950.0 * log(1023.0/float(analogRead(TEMP_SENSOR_1)) - 1.0)) - 273.15);
 }
-/**
- */
+
+
 void read_packet() {
     if (Serial.available()) {
         Serial.readStringUntil('\0').toCharArray(current_packet, 200);
     } 
 }
+
+
+void setup_sd() {
+    while (!SD.begin()) {}
+    log_file = SD.open("datalog.txt", FILE_WRITE);
+     if (log_file) {
+    log_file.println("FILE OK!!\n");
+    log_file.close();
+  }
+}
+
+
+void write_sd(char* packet) {
+    if (!new_data)
+        return;
+    log_file = SD.open("datalog.txt", FILE_WRITE);
+      if (log_file) {
+        log_file.write(packet, 200);
+        log_file.print("temperature,");
+        log_file.println(read_temp());
+        log_file.close();
+      }
+}
+
+void motor_setup(int STEP, int DIR, int nSLEEP) {
+  pinMode(STEP, INPUT);
+  pinMode(DIR, INPUT);
+  pinMode(nSLEEP, INPUT);
+}
+
+
 void setup() {
     Serial.begin(115200);
     STATES flight_status = PREFLIGHT;
+    setup_sd();
     //Thermal Setup
     pinMode(TEMP_SENSOR_1, INPUT);
     pinMode(HEATER, OUTPUT);
@@ -59,16 +85,23 @@ void loop() {
         case PREFLIGHT:
             read_packet()
             temp_reg();
+            write_sd(current_packet);
+            if (current_packet[0] == COAST_START)
+                flight_status = MICROG;
         break;
 
         case MICROG:
             read_packet()
             temp_reg();
+            write_sd(current_packet);
+            if (current_packet[0] == COAST_END)
+                flight_status = MICROG;
         break;
 
         case POSTFLIGHT:
             read_packet()
             temp_reg();
+            write_sd(current_packet);
         break;
     }
 }
---
+++ hardware/new_controller/definitions.h
@@ -0,0 +1,15 @@
+#define NONE '@'
+#define LIFTOFF 'A'
+#define MECO 'B '
+#define SEPARATION 'C'
+#define COAST_START 'D'
+#define APOGEE 'E'
+#define COAST_END 'F'
+#define UNDERCHUTES 'G'
+#define LANDING 'H'
+#define SAFING 'I'
+#define FINISHED 'J'
+
+#define FIVE_VOLTS 972.0
+#define HEATER A5;
+#define TEMP_SENSOR_1 A0;
